df294c8a9766d344cb9ee9230d28bec24de20191
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-09-28 16:17:26 +0200

    Update so order matter in Lab1-2
    Co-Authored-By: Pontus  <60647163+pontaberglund@users.noreply.github.com>

--- Lab 1/2.pl
+++ Lab 1/2.pl
@@ -1,15 +1,17 @@
 selectS(X,[X|T],T).
 selectS(X,[Y|T],[Y|R]) :- selectS(X,T,R).
 
+%Check if the element is a member of the list
 memberS(X,L) :- selectS(X,L,_).
 
-notS(X).
+%Reverse the inputted list
+reverse_list([],[]).
+reverse_list([Head|Tail],R) :- reverse_list(Tail,RT), append(RT, [Head], R).
 
-notS(X) :- X, !, fail.
+%Reverse the list, then remove elements and then reverse it again to keep the original order
+remove_duplicates([Head|Tail], R) :- reverse_list([Head|Tail], R1), remove_element(R1, R2), reverse_list(R2,R).
 
-remove_first([Head|Tail],Tail).
-
-remove_duplicates([],[]).
-
-remove_duplicates([Head|Tail],R) :- memberS(Head,Tail), remove_duplicates(Tail, R).
-remove_duplicates([Head|Tail],[Head|R]) :- remove_duplicates(Tail, R).
+%Remove all duplicate elements, checking with head and keeping head
+remove_element([],[]).
+remove_element([Head|Tail],R) :- memberS(Head,Tail), remove_element(Tail, R).
+remove_element([Head|Tail],[Head|R]) :- remove_element(Tail, R).
