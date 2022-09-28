5d2c268d40cee5d787ca2ab29d105a683dde5a10
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-09-28 11:53:14 +0200

    Add Lab 1-1
    Co-Authored-By: Pontus  <60647163+pontaberglund@users.noreply.github.com>

---
+++ Lab 1/1.pl
@@ -0,0 +1,9 @@
+T=f(a,Y,Z), T=f(X,X,b).
+
+% T = f(a, a, b),
+% Y = X, X = a,
+% Z = b.
+
+% a = X och eftersom X = Y så är Y = a 
+% b unifieras med Z
+% Vi kan därför skriva T=f(a,a,b) genom att byta ut X och Y.
