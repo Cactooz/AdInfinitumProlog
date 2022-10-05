0f86beed1f9b91eb6e06af96b8db514647847c6a
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-05 10:42:47 +0200

    Add appendS instead of the built in version

--- Lab 1/2.pl
+++ Lab 1/2.pl
@@ -1,12 +1,15 @@
 selectS(X,[X|T],T).
 selectS(X,[Y|T],[Y|R]) :- selectS(X,T,R).
 
+appendS([],L,L).
+appendS([H|T],L,[H|R]) :- appendS(T,L,R).
+
 %Check if the element is a member of the list
 memberS(X,L) :- selectS(X,L,_).
 
 %Reverse the inputted list
 reverse_list([],[]).
-reverse_list([Head|Tail],R) :- reverse_list(Tail,RT), append(RT, [Head], R).
+reverse_list([Head|Tail],R) :- reverse_list(Tail,RT), appendS(RT, [Head], R).
 
 %Reverse the list, then remove elements and then reverse it again to keep the original order
 remove_duplicates([Head|Tail], R) :- reverse_list([Head|Tail], R1), remove_element(R1, R2), reverse_list(R2,R).
