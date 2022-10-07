50de2fc0ca1abeed7b771b1e886ff85496924333
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-07 14:40:56 +0200

    Rename remove_element to remove_elements lab1-2

--- Lab 1/2.pl
+++ Lab 1/2.pl
@@ -12,9 +12,15 @@ reverse_list([],[]).
 reverse_list([Head|Tail],R) :- reverse_list(Tail,RT), appendS(RT, [Head], R).
 
 %Reverse the list, then remove elements and then reverse it again to keep the original order
-remove_duplicates([Head|Tail], R) :- reverse_list([Head|Tail], R1), remove_element(R1, R2), reverse_list(R2,R).
+remove_duplicates([Head|Tail], R) :- reverse_list([Head|Tail], R1), remove_elements(R1, R2), reverse_list(R2,R).
 
-%Remove all duplicate elements, checking with head and keeping head
-remove_element([],[]).
-remove_element([Head|Tail],R) :- memberS(Head,Tail), remove_element(Tail, R).
-remove_element([Head|Tail],[Head|R]) :- remove_element(Tail, R).
+%Remove all duplicate elements, checking with head and removing it if the tail has it
+remove_elements([],[]).
+
+%Remove the head if it exsists in the tail,
+%stop backtracking since the recursion takes care of removing the rest of the duplicates
+remove_elements([Head|Tail],R) :- memberS(Head,Tail), !, remove_elements(Tail, R).
+
+%If the head does not exsist
+%Go in another step in the list and do the same task, using recrusion
+remove_elements([Head|Tail],[Head|R]) :- remove_elements(Tail, R).
