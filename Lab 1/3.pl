30a4336d2fd79d71a610e4d1aa53ab887e14a1b0
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-07 14:39:43 +0200

    Add explaining comment to lab1-3

--- Lab 1/3.pl
+++ Lab 1/3.pl
@@ -10,3 +10,21 @@ split(List, Result) :- appendS(_, Part, List), appendS(Start, [End|_], Part), ap
 
 %Get the different parts and length of the list
 partstring(List, L, F) :- split(List, F), lengthS(F, L).
+
+/*
+?- partstring([1,2,3,4], L, F)
+appendS(_, Part, [1,2,3,4]) - Unify _ = [] and Part = [1,2,3,4]
+appendS(Start, [End|_], Part) - Unify Start = [], [End|_] = [1,2,3,4] with End = 1
+appendS(Start, [End], Result) - Append Start with [End] and add in result, like [] + [1] = [1]
+
+In the next step, it finds for solutions in split using another Start
+_ = [] and Part = [1,2,3,4]
+Start = [1] and [End|_] = [2,3,4] where End = 2
+[1] + [2] = [1,2]
+
+After that it is still _ = [] but Start = [1,2] and so on
+After all combinations for Start, it tests _ = [1] och gets
+_ = [1] and Part = [2,3,4]
+Start = [] and [End|_] = [2,3,4] where End = 2
+[] + [2] = [2] 
+*/
