829d9eeba7b6f4aaaa3b8fc9d9ae9be8b9e9d560
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-10-31 17:09:59 +0100

    Add base operations
    Co-Authored-By: Pontus  <60647163+pontaberglund@users.noreply.github.com>

--- Lab 2/proofcheck.pl
+++ Lab 2/proofcheck.pl
@@ -1,6 +1,15 @@
 % Base case
 valid_proof(Premiss, Goal, Proof) :- member(Goal, Premiss).
 
-%Verify the data from the inputted file
+% Base operations
+and(X, Y) :- X, Y.
+
+or(X, Y) :- X; Y.
+
+not(X) :- X, !, fail; true.
+
+imp(X, Y) :- and(X, Y); not(X).
+
+% Verify the data from the inputted file
 verify(InputFileName) :- see(InputFileName), read(Premiss), read(Goal),
 						read(Proof), seen, valid_proof(Premiss, Goal, Proof).
