ab1bf671a08b811dc3c87a12b1a82c1fd04a7a16
Hugo <54435874+Cactooz@users.noreply.github.com>
2022-11-13 11:38:40 +0100

    Add predicates to check and save proof lines
    Co-Authored-By: Pontus  <60647163+pontaberglund@users.noreply.github.com>

--- Lab 2/proofcheck.pl
+++ Lab 2/proofcheck.pl
@@ -1,15 +1,40 @@
-% Base case
-valid_proof(Premiss, Goal, Proof) :- member(Goal, Premiss).
-
 % Base operations
 and(X, Y) :- X, Y.
 
-or(X, Y) :- X; Y.
+or(X, Y) :- X; Y. 
 
 neg(X) :- X, !, fail; true.
 
 imp(X, Y) :- and(X, Y); neg(X).
 
-% Verify the data from the inputted file
-verify(InputFileName) :- see(InputFileName), read(Premiss), read(Goal),
-						read(Proof), seen, valid_proof(Premiss, Goal, Proof).
+
+%Verify the data from the inputted file
+verify(InputFileName) :- see(InputFileName), read(Premise), read(Goal),
+						read(Proof), seen, validProof(Premise, Goal, Proof).
+
+%Check if the goal and proof is valid
+%validProof(Premise, Goal, Proof) :- member(Goal, Premise).
+validProof(Premise, Goal, Proof) :- goalCheck(Proof, Goal), checkProof(Proof, Premise, []), !.
+
+
+%Append line to end of list
+appendLast(X, [], [X]).
+appendLast(A, [Head|Tail], [Head|R]) :- appendLast(A, Tail, R).
+
+%Adds the checked line to the checked list
+addChecked(Line, Old, New) :- appendLast(Line, Old, New).
+
+
+%Go to the last line in the proof
+goToLastLine([X], X).
+goToLastLine([Head|Tail], NextLine) :- goToLastLine(Tail, NextLine).
+
+%Check if the last line of the proof is the goal
+goalCheck(Proof, Goal) :- goToLastLine(Proof, R), member(Goal, R), !.
+
+
+%Check if a line in the proof is correct and continue to the next line recursively
+checkProof([], _, _).
+checkProof([Head|Tail], Premise, Checked) :- ruleCheck(Head, Premise, Checked),
+											addChecked(Head, Checked, NewChecked),
+											checkProof(Tail, Premise, NewChecked).
